03617f24efc8603ca312eee2cd7e93e0b17b7e29
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-10-29 10:13:30 -0400

    Done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -3,12 +3,28 @@ package io.zipcoder;
 public class Music {
 
     private String[] playList;
-
+    
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int counter1 = 0;
+        int counter2 = 0;
+
+        for(int i = 0; i <= startIndex; i++){
+            counter1++;
+        }
+        for(int j = 0; j <= playList.length; j++){
+            counter2++;
+        }
+        if(counter1 > counter2){
+            return counter2;
+        }
+        else{
+            return counter1;
+        }
+
+
     }
 }
